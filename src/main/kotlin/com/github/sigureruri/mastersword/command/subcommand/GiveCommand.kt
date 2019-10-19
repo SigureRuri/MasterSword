0be0f2368352d1d6ca8564065f2518f461009cd0
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2019-10-19 18:11:24 +0900

    add: コマンドの基礎追加

---
+++ src/main/kotlin/com/github/sigureruri/mastersword/command/subcommand/GiveCommand.kt
@@ -0,0 +1,13 @@
+package com.github.sigureruri.mastersword.command.subcommand
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+
+class GiveCommand : CommandExecutor {
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}

571c8a99c8e8517e6c6b352daf967f5daa2093dc
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2019-10-19 17:14:30 +0900

    first

---
+++ src/main/kotlin/com/github/sigureruri/mastersword/MasterSword.kt
@@ -0,0 +1,12 @@
+package com.github.sigureruri.mastersword
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MasterSword : JavaPlugin() {
+
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+    }
+}
